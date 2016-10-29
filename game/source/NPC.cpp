aebf6ec7dc4f5f9cc255ba43123c8b66a07c26c1
Matthias Bruns <matthiasbruns@live.de>
2016-10-29 11:52:14 +0200

    Added npc

---
+++ game/source/NPC.cpp
@@ -0,0 +1,47 @@
+//
+// Created by Matthias Bruns on 29/10/2016.
+//
+
+#include "NPC.h"
+#include "PlayerEntity.h"
+#include "Dialog.h"
+
+namespace TT
+{
+    NPC::NPC(sf::Vector2f position) : Actor(position, "assets/textures/npc_00.png", b2_kinematicBody, true, sf::Vector2f(5.0f, 5.0f)) {
+    }
+
+    void NPC::Update(float timeStep) {
+        Actor::Update(timeStep);
+    }
+
+    void NPC::Draw(sf::RenderWindow *window) {
+        Actor::Draw(window);
+    }
+
+    void NPC::OnCollisionStart(b2Fixture *other) {
+        Actor::OnCollisionStart(other);
+
+        void *bodyUserData = other->GetUserData();
+        if (bodyUserData) {
+            PlayerEntity* player = static_cast<PlayerEntity *>( bodyUserData);
+            if(player) {
+                // Write dialog
+                Dialog::GetInstance()->SetText("TEST 123");
+            }
+        }
+    }
+
+    void NPC::OnCollisionExit(b2Fixture *other) {
+        Actor::OnCollisionExit(other);
+
+        void *bodyUserData = other->GetUserData();
+        if (bodyUserData) {
+            PlayerEntity* player = static_cast<PlayerEntity *>( bodyUserData);
+            if(player) {
+                // Write dialog
+                Dialog::GetInstance()->SetText("");
+            }
+        }
+    }
+}
---
+++ game/source/NPC.h
@@ -0,0 +1,28 @@
+//
+// Created by Matthias Bruns on 29/10/2016.
+//
+
+#ifndef TIMETRAVEL_NPC_H
+#define TIMETRAVEL_NPC_H
+
+#include <SFML/Graphics/RenderWindow.hpp>
+#include "Actor.h"
+
+namespace TT
+{
+	class NPC : public Actor {
+	public:
+        NPC(sf::Vector2f position);
+
+		void Update(float timeStep) override ;
+		void Draw(sf::RenderWindow *window) override;
+
+        void OnCollisionStart(b2Fixture* other) override;
+        void OnCollisionExit(b2Fixture* other) override;
+
+    private:
+
+	};
+}
+
+#endif //TIMETRAVEL_NPC_H
