2b4dfa56c3dcec506e4fa4933c177db03a06da3b
Nils Daumann <nils_daumann@slindev.com>
2016-10-29 13:05:47 +0200

    Perfect altar class, doing nothing.

---
+++ game/source/Altar.cpp
@@ -0,0 +1,28 @@
+//
+// Created by Nils Daumann on 29.10.16.
+//
+
+#include "Altar.h"
+
+namespace TT
+{
+	Altar::Altar()
+	{
+
+	}
+
+	Altar::~Altar()
+	{
+
+	}
+
+	void Altar::Update(float timestep)
+	{
+		
+	}
+
+	void Altar::Draw(sf::Window *_window)
+	{
+
+	}
+}
