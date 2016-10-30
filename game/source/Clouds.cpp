4a0b7d3e2731b082a73799f90bb01810dd5bb329
filipo55 <cel.filip@gmail.com>
2016-10-30 14:09:33 +0100

    Clouds finally working

--- game/source/Clouds.cpp
+++ game/source/Clouds.cpp
@@ -4,12 +4,12 @@ namespace TT {
 
 	Clouds::~Clouds()
 	{
-
 	}
 
 	Clouds::Clouds(float offsetspeed, float speed, const std::string path) : Background(speed, path), _offset(0.0f), _offsetspeed(offsetspeed)
 	{
-		
+		_object->setOrigin(0.0, _object->getOrigin().y);
+		xproblematic = 0.0f;
 	}
 
 	void Clouds::Draw(sf::RenderWindow *window)
@@ -18,21 +18,14 @@ namespace TT {
 		{
 			return;
 		}
-
-/*		sf::Vector2f center;
+		sf::Vector2f center;
 		center = window->getView().getCenter();
 		float width = (float)window->getSize().x;
-
-		if(_object->getPosition().x > center.x + 5757)
-		{
-			_object->setPosition(center.x - width, 0.0f);
-			_offset = 0.0f;
-		}*/
-
-		sf::Vector2f center = window->getView().getCenter();
-		float width = (float)window->getSize().x;
+		if (_offset > 5757.0f) _offset = 0;
 		_object->setPosition(center.x * GetSpeed() + _offset, 0.0f);
-		
+		xproblematic = _object->getPosition().x;
+		window->draw(*_object);
+		_object->setPosition(xproblematic -5757.0, 0.0f);
 		window->draw(*_object);
 	}
 
